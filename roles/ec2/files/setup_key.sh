6892faba91bb0396c3cd539b6cd62b7738962685
Prashant Patankar <plpatankar@gmail.com>
2018-02-27 12:12:50 +0000

    Updated setup_ssh.yml

--- roles/ec2/files/setup_key.sh
+++ roles/ec2/files/setup_key.sh
@@ -7,16 +7,20 @@ fi
 
 key=$1
 host=$2
+usr='root'
 
-curr_pub_key=`sudo cat ~/.ssh/id_rsa.pub`;
+curr_pub_key=`sudo cat ~/.ssh/id_rsa.pub 1>&2 2>/dev/null`;
+#curr_pub_key=$(<~/.ssh/id_rsa.pub)
 
 #ssh -i "${key}" ec2-user@${host} "sudo su; if grep -q \"${curr_pub_key}\" ~/.ssh/known_hosts; then; echo 'Key exist already'; else; echo ${curr_pub_key} >> ~/.ssh/known_hosts; fi"
 ssh -i "${key}" ec2-user@${host} << EOF
-   if grep -q "${curr_pub_key}" ~/.ssh/authorized_keys; then
+   home_dir=$( eval echo ~$usr )
+   echo ${home_dir};
+   if grep -q ${curr_pub_key} ${home_dir}/.ssh/authorized_keys; then
       echo "Key exist already";
    else
-      echo "${curr_pub_key}"| sudo tee -a ~/.ssh/authorized_keys;
+      echo ${curr_pub_key}| sudo tee -a ${home_dir}/.ssh/authorized_keys;
    fi
-EOF
+EOF 
 
 
